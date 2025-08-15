aa7d3c45c29ced7f228dc3ddbc3570b7a786cb0d
Juan Manuel Moreno Guevara <morenoguevarajuan20@gmail.com>
2025-08-14 21:54:47 -0500

    .

---
+++ intro/index.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Invitación intro</title>
+  <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+
+
+<a href="#" id="openBtn" class="fondo-link"></a>
+
+  <!-- Contenedor centrado -->
+  <div class="center-container">
+    <div id="videoContainer" class="video-hidden">
+       <!-- Video escritorio -->
+  <video id="videoDesktop" autoplay muted playsinline>
+    <source src="../video/intro.mp4" type="video/mp4">
+    Tu navegador no soporta videos.
+  </video>
+
+  <!-- Video móvil -->
+  <video id="videoMobile" autoplay muted playsinline>
+    <source src="../video/intro_vertical.mp4" type="video/mp4">
+    Tu navegador no soporta videos.
+  </video>
+    </div>
+  </div>
+
+  <script>
+  const btn = document.getElementById("openBtn");
+  const videoContainer = document.getElementById("videoContainer");
+
+  // Detectar si es móvil
+  const esMovil = window.matchMedia("(max-width: 768px)").matches;
+
+  // Seleccionar el video correcto
+  const video = esMovil
+    ? document.getElementById("videoMobile")
+    : document.getElementById("videoDesktop");
+
+  document.addEventListener("contextmenu", e => e.preventDefault());
+
+  btn.addEventListener("click", () => {
+    btn.classList.add("flash");
+    setTimeout(() => {
+      btn.style.display = "none";
+      videoContainer.classList.remove("video-hidden");
+      videoContainer.classList.add("video-show");
+      video.play();
+    }, 800);
+  });
+
+  video.addEventListener("ended", () => {
+    videoContainer.classList.remove("video-show");
+    videoContainer.classList.add("flash");
+    setTimeout(() => {
+      window.location.href = "../invitacion/";
+    }, 800);
+  });
+</script>
+
+
+
+<script>
+document.getElementById("openBtn").addEventListener("click", function() {
+    document.getElementById("musicFrame").contentWindow.postMessage('playMusic', '*');
+});
+</script>
+
+
+</body>
+</html>
